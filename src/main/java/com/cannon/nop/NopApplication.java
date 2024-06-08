a0124edc0f1c8efe904827b3229e906dce04ef95
tmdwlsla123 <kwag57@naver.com>
2024-06-08 19:44:38 +0900

    Create NOP project

---
+++ src/main/java/com/cannon/nop/NopApplication.java
@@ -0,0 +1,13 @@
+package com.cannon.nop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cannon/nop/NopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cannon.nop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
